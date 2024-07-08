7762498c5addcf953332390b29c58f3673990e5a
bryanachmad <bryanachmad08@gmail.com>
2024-07-08 23:49:37 +0700

    change code

--- index.js
+++ index.js
@@ -156,4 +156,4 @@ app.get("/", (req, res) => {
 	res.send({ message: "Halooo" });
 });
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
