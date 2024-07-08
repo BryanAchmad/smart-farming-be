3788a9efaa3f0b42b260edac7743ec88e6359f96
bryanachmad <bryanachmad08@gmail.com>
2024-07-08 23:22:53 +0700

    delet controller

--- controller/dataset.js
+++
@@ -1,9 +0,0 @@
-const app = require("express");
-const router = app.Router();
-
-const express_jwt = require("express-jwt");
-const jwt = express_jwt({
-	secret: process.env.JWT_SECRET,
-	algorithms: ["HS256"],
-});
-
