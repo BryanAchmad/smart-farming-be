40bf88127d366ba426ce66fb4310bbace81e0e5c
bryanachmad <bryanachmad08@gmail.com>
2024-07-11 21:29:52 +0700

    local

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["*","https://smart-farming-virid.vercel.app"],
+		origin: ["http://localhost:5173","https://smart-farming-virid.vercel.app"],
 		methods: ["*"],
 	},
 });
@@ -36,7 +36,7 @@ mongoose
 
 app.use(
     cors({
-        origin: ["*","https://smart-farming-virid.vercel.app"], // Allow the specific origin of your frontend
+        origin: ["http://localhost:5173","https://smart-farming-virid.vercel.app"], // Allow the specific origin of your frontend
         methods: ["*"], // Allow all methods
     })
 );
