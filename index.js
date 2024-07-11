90f7f61c515f5bfe329144ef0a2e71f0662bd4ab
bryanachmad <bryanachmad08@gmail.com>
2024-07-11 17:24:41 +0700

    add cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: ["*", "https://smart-farming-virid.vercel.app/"],
 		methods: ["GET", "POST"],
 	},
 });
